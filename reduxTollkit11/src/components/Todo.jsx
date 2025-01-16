b55a187011b421ab1dc37e7be23e3c74d5341462
chowdhuryamit <camit8546@gmail.com>
2025-01-16 19:15:03 +0530

    setup

--- reduxTollkit11/src/components/Todo.jsx
+++ reduxTollkit11/src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector,useDispatch } from "react-redux";
-import { removeTodo } from "../features/todo/todoSlice";
+import { removeTodo} from "../features/todo/todoSlice";
 
 function Todo(){
     const todos=useSelector((state)=>state.todos);
