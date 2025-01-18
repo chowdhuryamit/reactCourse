9df1b87b467506337ba878fec698f6013772e3d6
chowdhuryamit <camit8546@gmail.com>
2025-01-18 15:03:10 +0530

    appwrite setup

---
+++ reactBolgProject12/src/appwrite/auth.js
@@ -0,0 +1,54 @@
+import conf from "../conf/conf";
+import {Client,Account,ID} from "appwrite"
+
+class AuthService{
+    client=new Client();
+    account
+    constructor(){
+        this.client.setEndpoint(conf.appwriteurl).setProject(conf.appwriteprojectid);
+        this.account=new Account(this.client);
+    }
+
+    async createAccount({name,email,password}){
+      try {
+        const userAccount=await this.account.create(ID.unique(),name,email,password);
+        if(userAccount){
+            //call login method
+            return this.login(email,password);
+        }
+        else{
+          return userAccount;
+        }
+      } catch (error) {
+        throw error;
+      }
+    }
+
+    async login({email,password}){
+        try {
+            return await this.account.createEmailSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService=new AuthService();
+
+export default authService
